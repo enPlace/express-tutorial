4cf27ff6076b94e06693857740d3430a4698a2c9
enPlace <n.place@protonmail.com>
2021-06-09 15:24:18 -0300

    why express is good

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const http = require('http')
+const fs = require('fs')
+
+//get all files
+const homePage = fs.readFileSync('./navbar-app/index.html') //this only runs when we instantiate the server, not each time it is called. 
+
 
 const server = http.createServer((req,res)=>{
     //console.log(req.method)
     if(req.url ==='/'){
     res.writeHead(200, {'content-type':'text/html'})
-    res.write("<h1>homies page</h1>")
+    res.write(homePage)
     res.end()
     }else if (req.url === '/about'){
+        res.end()
 
     }else{
         res.writeHead(404, {'content-type':'text/html'})
